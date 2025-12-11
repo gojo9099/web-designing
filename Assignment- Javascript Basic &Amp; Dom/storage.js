6f544ace593f20672b577e38063536cbb26caa25
gojo9099 <vikashshakya2486@gmail.com>
2025-12-11 11:57:02 +0530

    update and new folder and files add

--- Assignment- Javascript Basic &Amp; Dom/storage.js
+++
@@ -1,5 +0,0 @@
-export function saveData(data) {
-  console.log("📌 User Submitted Data:", data);
-  localStorage.setItem("registrationData", JSON.stringify(data));
-  return "Data saved successfully (check console).";
-}
